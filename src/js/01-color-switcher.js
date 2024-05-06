aca75d5ea321e396773998320623a09e1203fccf
Merlyin <focastefan13@gmail.com>
2024-05-06 22:39:30 +0300

    test1
    color

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,23 @@
+const startBtn = document.querySelector('button[data-start]');
+const stopBtn = document.querySelector('button[data-stop]');
 
+let intervalId = null;
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+function startColorSwitching() {
+  startBtn.disabled = true;
+  intervalId = setInterval(() => {
+    document.body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+}
+
+function stopColorSwitching() {
+  clearInterval(intervalId);
+  startBtn.disabled = false;
+}
+
+startBtn.addEventListener('click', startColorSwitching);
+stopBtn.addEventListener('click', stopColorSwitching);
